1f987f60f033ecefe6d3a06f12c88b70198a17c8
GitThisJoel <joelbacke10@gmail.com>
2021-11-03 10:42:08 +0100

    added a gitignore

---
+++ lab1/hello_world.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void) {
+  // task 1
+  // printf("hello world\n");
+
+  // task 6 - 7
+  int m; int n;
+  scanf("%d\n%d", &m, &n); // same as: scanf("%d", &m); scanf("%d", &n);
+  printf("m = %d, n = %d\n", m, n);
+  return 0;
+}
