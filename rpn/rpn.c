7ae010f70b0fde6ae2b8fb6583dfeeb853716952
GitThisJoel <joelbacke10@gmail.com>
2021-11-09 20:40:24 +0100

    first working part, completes the logic for test 1

--- rpn/rpn.c
+++ rpn/rpn.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
-
+#include <stdlib.h>
+#include <ctype.h>
 #define N (10)
 
 int main(void)
@@ -17,8 +18,15 @@ int main(void)
 			// if c is \n => print out number if only one on stack else error
 			// if c is undefined => print error
 
-			printf("%c", c);
+			// printf("%c", c);
 			c = getchar();
+			if (isdigit(c)){
+				stack[on_stack] = c-'0';
+				on_stack++;
+				printf(" %d", stack[on_stack-1]);
+			} else if (c == '+') {
+				printf(" %d", stack[on_stack-2]+stack[on_stack-1]);
+			}
 		}
 	c = getchar();
 	}
